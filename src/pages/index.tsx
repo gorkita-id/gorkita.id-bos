10dc7182d18e41fe78006a876afb5b5ed1f5d0f4
alvinisasi <azraqazravin@gmail.com>
2023-10-21 16:33:49 +0700

    add: sample navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { useRouter } from "next/navigation"
+
 export default function Page() {
+    const router = useRouter()
+
     return (
         <main className="flex flex-col justify-center items-center min-h-screen">
             <h1>Hello, Next.js!</h1>
-            <button className="btn">Hello daisyUI</button>
+            <button className="btn" onClick={() => router.push('/login')}>Go to Login</button>
         </main>
     )
 }
