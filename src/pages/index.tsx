5ef5e179c1249109730d3b46d24772d612f13775
alvinisasi <azraqazravin@gmail.com>
2023-10-21 16:26:54 +0700

    setup: /pages, daisyui

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main className="flex flex-col justify-center items-center min-h-screen">
+            <h1>Hello, Next.js!</h1>
+            <button className="btn">Hello daisyUI</button>
+        </main>
+    )
+}
