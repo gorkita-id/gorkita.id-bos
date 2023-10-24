3393166f12dcddea252c28011d718243981e2e07
alvinisasi <azraqazravin@gmail.com>
2023-10-24 22:05:53 +0700

    update: forms, image optimization

---
+++ src/types/component.types.ts
@@ -0,0 +1,16 @@
+export interface TextInputPropsType {
+    name: string;
+    label: string;
+    type: string;
+    onChange: any; 
+    onBlur: any;
+    value: string;
+    className?: string;
+}
+
+export interface ButtonPropsType {
+    label: string;
+    onClick: any; 
+    type: "button" | "submit" | "reset";
+    className?: string;
+}
