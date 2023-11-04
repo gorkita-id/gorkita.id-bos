22ce3d44558a22356f5e1dd9b0f3547ddb02b84f
alvinisasi <azraqazravin@gmail.com>
2023-11-05 00:41:20 +0700

    add: schedules, fields routes

---
+++ src/app/(auth)/dashboard/fields/page.tsx
@@ -0,0 +1,9 @@
+const Fields: React.FC = () => {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold my-4">Lapangan</h1>
+        </div>
+    )
+}
+
+export default Fields
---
+++ src/app/(auth)/dashboard/schedules/page.tsx
@@ -0,0 +1,9 @@
+const Schedules: React.FC = () => {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold my-4">Jadwal</h1>
+        </div>
+    )
+}
+
+export default Schedules
